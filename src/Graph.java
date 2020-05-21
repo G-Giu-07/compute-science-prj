0a44bbfe5ec36db7d7093de99300ec720a850dc6
G-Giu-07 <giulygrasso985@gmail.com>
2020-05-21 12:15:08 +0200

    feat(graph): add the code base to build the graph

---
+++ src/Graph.java
@@ -0,0 +1,99 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Iterator;
+import java.util.Set;
+import java.util.HashSet;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+class Graph  {
+  private int N; // number of vertex
+  private ArrayList<LinkedList<Integer>> adjList = new ArrayList<LinkedList<Integer>>();  // adjacency list
+
+  Graph() { }
+
+  Graph(int n) {
+    this.initGraph(n);
+  }
+
+  /* This function initialize the adjacency list */
+  private void initGraph(int n) {
+    this.N = n;
+    for (int i = 0; i < n; ++i) {
+      this.adjList.add(new LinkedList<Integer>());
+    }
+  }
+
+  // function to add an edge between two nodes
+  public void addEdge(int v1,int v2)  {
+    this.adjList.get(v1).add(v2);
+    this.adjList.get(v2).add(v1);
+  }
+
+  public void printGraph() {
+    for (int i = 0; i < this.adjList.size(); i++) {
+      Iterator<Integer> j = this.adjList.get(i).listIterator();
+      while (j.hasNext()) {
+        int n = j.next();
+        System.out.println(i + ", " + n);
+      }
+    }
+
+    System.out.println("N: " + this.N);
+  }
+
+  public static void main(String[] args) {
+
+    Graph g = GraphIO.read("only_IDs.txt");
+
+    // g.printGraph();
+  }
+
+}
+
+class GraphIO {
+  static Graph read(String filename) {
+    ArrayList<Integer> edges = new ArrayList<Integer>();
+    Set<Integer> vertex = new HashSet<Integer>();
+
+
+    // read the file and save the vertex into "edges"
+    try {
+
+      // read the file
+      File file = new File(filename);
+      Scanner reader = new Scanner(file);
+
+      while (reader.hasNextLine()) {
+        String data = reader.nextLine();
+
+        // read each line and split it asing on the following structure "v1,v2"
+        // where v1 -> v2 is the edge
+        String[] edge = data.replaceAll("\\s+","").split(",");
+
+        int e1 = Integer.parseInt(edge[0]);
+        int e2 = Integer.parseInt(edge[1]);
+        vertex.add(e1);
+        vertex.add(e2);
+        edges.add(e1);
+        edges.add(e2);
+      }
+
+      reader.close();
+    } catch (FileNotFoundException e) {
+      System.out.println("File not found!");
+
+      e.printStackTrace();
+      return null;
+    }
+
+    // generate the graph using the "edges" array
+    Graph g = new Graph(vertex.size());
+    for (int i = 0; i < edges.size(); i += 2) {
+      g.addEdge(edges.get(i), edges.get(i + 1));
+    }
+
+    return g;
+  }
+}
--- src/Preprocessing.java
+++ src/Preprocessing.java
@@ -22,8 +22,11 @@ public class Preprocessing {
         BufferedReader br = new BufferedReader(new FileReader(file));
         File output = new File("output.txt");
         BufferedWriter writer = null;
+        BufferedWriter onlyIDs = null;
+
         try {
          writer = new BufferedWriter(new FileWriter(output));
+         onlyIDs = new BufferedWriter(new FileWriter("only_IDs.txt"));
         } catch(Exception e) {}
 
         try {
@@ -33,31 +36,36 @@ public class Preprocessing {
           while ((str = br.readLine()) != null) {
             String[] fields = str.split("\t", -1);
 
-            // Contigue 1
+            // Contiguous 1
             String identifier1 = fields[0];           // Identifier
             int s1    = Integer.parseInt(fields[5]);  // StartOverlap
             int e1    = Integer.parseInt(fields[6]);  // EndOverlap
             int size1 = Integer.parseInt(fields[7]);  // Size
-            // Contigue 2
+            // Contiguous 2
             String identifier2 = fields[1];           // Identifier
             int s2    = Integer.parseInt(fields[9]);  // StartOverlap
             int e2    = Integer.parseInt(fields[10]); // EndOverlap
             int size2 = Integer.parseInt(fields[11]); // Size
 
-            if (!map.contains(identifier1)) {
-              map.add(identifier1);
-              dict.put(identifier1, idx);
-              idx++;
-            }
-
-            if (!map.contains(identifier2)) {
-              map.add(identifier2);
-              dict.put(identifier2, idx);
-              idx++;
-            }
-
+            // check if the contiguous overlaps do not contain themselves
             if (e1-s1 != size1 && e2-s2 != size2) {
+
+              // convert identifier1 into ID
+              if (!map.contains(identifier1)) {
+                map.add(identifier1);
+                dict.put(identifier1, idx);
+                idx++;
+              }
+  
+              // convert identifier2 into ID
+              if (!map.contains(identifier2)) {
+                map.add(identifier2);
+                dict.put(identifier2, idx);
+                idx++;
+              }
+
               writer.write(dict.get(identifier1) + ",	" + dict.get(identifier2) + ",	" + str + "\n");
+              onlyIDs.write(dict.get(identifier1) + ",	" + dict.get(identifier2) + "\n");
             }
 
             // i++;
@@ -69,6 +77,7 @@ public class Preprocessing {
 
           br.close();
           writer.close();
+          onlyIDs.close();
         } catch (IOException e) {
           System.out.println("IOException");
         }
